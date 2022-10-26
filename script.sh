a00e5bd3a90d24ed3bdf1bda629a173eaa8fbf64
Daphne <34064919+dvginneken@users.noreply.github.com>
2022-10-26 11:36:23 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,6 @@
 
 output_dir="$1"
 haplohiv_folder="$2"
-table="mastertable.tsv"
 
 # Retrieve the name and locations of the sample consensus files to be taken together (replicates)
 mkdir -p ${output_dir}/consensus
@@ -11,10 +10,10 @@ if [ "$(ls -A ${output_dir}/consensus)" ]; then
 else
     for sample in `ls ${haplohiv_folder}/ec`
     do
-        Rscript ConsensusFiles.R $table $haplohiv_folder $output_dir $sample
+        Rscript ConsensusFiles.R "mastertable.tsv" $haplohiv_folder $output_dir $sample
     done
 fi
 
 # Run the haplotyping pipeline
-snakemake -j 8 --config output_dir=$output_dir haplohiv_folder=$haplohiv_folder table=$table --
+snakemake -j 8 --config output_dir=$output_dir haplohiv_folder=$haplohiv_folder --
 
